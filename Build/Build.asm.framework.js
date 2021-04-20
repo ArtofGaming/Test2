f22b3b0484341cccd239c42551f6827c2af08101
ArtofGaming <turner4965@gmail.com>
2021-04-19 20:20:51 -0700

    .

--- Build/Build.asm.framework.js
+++ Build/Build.asm.framework.js
@@ -1019,7 +1019,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2417232;
+STATICTOP = STATIC_BASE + 2417376;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
